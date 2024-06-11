93b25ceb03a943339c666c34c67775dfe65c2cab
maluhuillcaelguera <maluhuillcaelguera@gmail.com>
2024-06-11 13:40:53 -0500

    uploading integration of react components, installing Jest Babel, Jest JSDOM environment y Test library

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 export default {
-    setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+   
     testEnvironment: 'jsdom',
     transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './babel.config.js' }],
+      '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './babel.config.json' }],
     },
     testMatch: ['**/__tests__/**/*.test.(js|jsx|ts|tsx)', '**/?(*.)+(spec|test).(js|jsx|ts|tsx)'],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
-/* import React from 'react';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import {expect, test} from '@jest/globals';
 
 test('renders welcome message', () => {
   render(<App />);
   const welcomeMessage = screen.getByText(/welcome/i);
   expect(welcomeMessage).toBeInTheDocument();
-});*/
+});
