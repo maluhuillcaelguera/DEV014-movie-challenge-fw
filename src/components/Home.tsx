14e5b60610a771b0e8a87e6d508349347755a325
maluhuillcaelguera <maluhuillcaelguera@gmail.com>
2024-06-25 11:53:31 -0500

    Integrate the Pagination component using the onSelectPage prop. Every time the user clicks a page button, update the value of the query param currentPage from the URL to the new page. Also, call the getMovies service every time you change the query param currentPage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import MovieList from './MovieList';
 import { Movie } from '../models/Movie';
 import { APIService } from '../services/movies';
-import '../styles/Home.css'; 
+import Pagination from './Pagination';
+import '../styles/Home.css';
 
 const Home: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(1);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = parseInt(searchParams.get('currentPage') || '1', 10);
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const response = await APIService.getMovies({ filters: { page: currentPage } });
         setMovies(response.movies);
-        setCurrentPage(response.metaData.pagination.currentPage);
         setTotalPages(response.metaData.pagination.totalPages);
       } catch (error) {
         if (error instanceof Error) {
@@ -32,6 +35,10 @@ const Home: React.FC = () => {
     fetchMovies();
   }, [currentPage]);
 
+  const handlePageSelect = (page: number) => {
+    setSearchParams({ currentPage: page.toString() });
+  };
+
   if (loading) {
     return <div>Cargando...</div>;
   }
@@ -44,15 +51,7 @@ const Home: React.FC = () => {
     <div className="Home">
       <h1>Mi Catálogo de Películas</h1>
       <MovieList movies={movies} />
-      <div>
-        <button onClick={() => setCurrentPage((prevPage) => Math.max(prevPage - 1, 1))}>
-          Anterior
-        </button>
-        <span>{currentPage}</span>
-        <button onClick={() => setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages))}>
-          Siguiente
-        </button>
-      </div>
+      <Pagination currentPage={currentPage} totalPages={totalPages} onSelectPage={handlePageSelect} />
     </div>
   );
 };
