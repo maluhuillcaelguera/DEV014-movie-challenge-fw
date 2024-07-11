f6e879f2a45c66670e26c5cdcb0234060124ce85
maluhuillcaelguera <maluhuillcaelguera@gmail.com>
2024-07-11 12:50:04 -0500

    Add: configuracion el archivo file-mocks.js

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
   moduleNameMapper: {
     "^.+\\.css$": "<rootDir>/src/__mocks__/file-mock.cjs",
     "^@/(.*)$": "<rootDir>/src/$1",
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/file-Mock.cjs',
   },
   preset: 'ts-jest',
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
