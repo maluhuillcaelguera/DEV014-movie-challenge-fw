f764f8eabfd9de81c91bdc87b45ab43d681c1654
maluhuillcaelguera <maluhuillcaelguera@gmail.com>
2024-07-10 22:37:06 -0500

    feat: Add formatMovie, formatGenresToMap, and formatGenresToOptions utilities

--- src/utils/transformers.ts
+++ src/utils/transformers.ts
@@ -1,8 +1,11 @@
 import { Movie } from '../models/Movie';
 
 export const formatMovie = (data: any, genreMap: Map<number, string>): Movie => {
-  const genres = data.genre_ids.map((id: number) => genreMap.get(id) || 'Unknown Genre');
-
+  // const genres = data.genre_ids && data.genre_ids.map((id: number) => genreMap.get(id) || 'Unknown Genre');
+  const genres = Array.isArray(data.genres)
+    ? data.genres.map((genre: { id: number, name: string }) => genreMap.get(genre.id) || genre.name || 'Unknown Genre')
+    :data.genre_ids ? data.genre_ids.map((id: number) => genreMap.get(id) || 'Unknown Genre') : [];
+console.log(genreMap)
   return {
     id: data.id,
     poster: data.poster_path ? `https://image.tmdb.org/t/p/w500${data.poster_path}` : '',
@@ -10,7 +13,7 @@ export const formatMovie = (data: any, genreMap: Map<number, string>): Movie => 
     overview: data.overview,
     releaseYear: data.release_date ? new Date(data.release_date + "T00:00:00").getFullYear() : 0,
     voteAverage: data.vote_average,
-    genres: genres,  // Utilizar los nombres de los géneros
+    genres: genres,  // Utilizar los nombres de los géneros o un arreglo vacío si no hay datos
   };
 };
 
