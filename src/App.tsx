8d201e0170c7963d1d39df10635928dc278c5419
maluhuillcaelguera <maluhuillcaelguera@gmail.com>
2024-07-10 22:36:28 -0500

    feat: Set up routing in App component with Home and MovieDetail components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 // App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import MovieDetail from './components/MovieDetail';
+import { APIService } from './services/movies'; // Asegúrate de importar correctamente
+import { formatGenresToMap } from './utils/transformers';
 import './App.css';
 
 const App: React.FC = () => {
+  const [genreMap, setGenreMap] = useState<Map<number, string>>(new Map());
+
+  useEffect(() => {
+    const fetchGenres = async () => {
+      try {
+        const genres = await APIService.getMovieGenres();
+        setGenreMap(formatGenresToMap(genres));
+      } catch (err) {
+        console.error('Error fetching genres:', err);
+      }
+    };
+
+    fetchGenres();
+  }, []);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<MovieDetail genreMap={genreMap} />} />
       </Routes>
     </Router>
   );
