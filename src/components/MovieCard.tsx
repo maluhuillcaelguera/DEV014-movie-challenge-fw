646bf83e06c49fb160d07e08f240ae077de0ccd1
maluhuillcaelguera <maluhuillcaelguera@gmail.com>
2024-07-10 22:33:31 -0500

    feat: Add MovieCard component with navigation and display of movie details

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Movie } from '../models/Movie';
+import { useNavigate } from 'react-router-dom';
 import '../styles/MovieCard.css';
 
 interface MovieCardProps {
@@ -7,22 +8,39 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
-  const { title, poster, releaseYear, genres } = movie;
+  const { id, title, poster, releaseYear, genres, voteAverage } = movie;
+  const navigate = useNavigate();
+
+  
+
+  const handleNavigate = () => {
+    navigate(`/movie/${id}`);
+  };
 
   return (
-    <div className="movie-card">
+    <div className="movie-card" >
       <img src={poster || "https://example.com/no-image.jpg"} alt={title || "No Title"} className="poster-image" />
-      <h3>{title || "No Title"}</h3>
-      <p>{releaseYear || "No Year"}</p>
-      <div className="genres">
-        {genres && genres.length > 0 && (
-          <ul>
-            {genres.map((genre, index) => (
-              <li key={index}>{genre}</li>
-            ))}
-          </ul>
-        )}
-      </div>
+      { (
+        <div className="overlay">
+          <h3>{title || "No Title"}</h3>
+          <div className="year">
+            <p>Año: {releaseYear || "No Year"}</p>
+          </div>
+          <div className="Rating">
+            <p>Rating: {voteAverage || "No voteAverage"}</p>
+          </div>
+          <div className="genres">
+            {genres && genres.length > 0 && (
+              <ul>
+                {genres.map((genre, index) => (
+                  <li key={index}>{genre}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+          <button name= "Ver más" className="more" onClick={handleNavigate}>Ver más</button>
+        </div>
+      )}
     </div>
   );
 }
