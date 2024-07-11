7b7d1af5038a73dacee460600100e959107952b0
maluhuillcaelguera <maluhuillcaelguera@gmail.com>
2024-07-10 22:17:38 -0500

    updating configuration files

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,16 +1,16 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  preset: 'ts-jest',
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
-    "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
+    "^.+\\.(ts|js|tsx|jsx)$": "babel-jest",
   },
   transformIgnorePatterns: [
     "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
   ],
   moduleNameMapper: {
     "^.+\\.css$": "<rootDir>/src/__mocks__/file-mock.cjs",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   preset: 'ts-jest',
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
