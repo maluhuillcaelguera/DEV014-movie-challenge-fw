54b59375fd9c32c454bdc77ae15e89ee7695761d
maluhuillcaelguera <maluhuillcaelguera@gmail.com>
2024-07-11 13:01:02 -0500

    Add:Updating configurations

--- src/utils/transformers.ts
+++ src/utils/transformers.ts
@@ -1,15 +1,13 @@
 import { Movie } from '../models/Movie';
-import optionalPoster from '../assets/pelicula.jpeg';
 
 export const formatMovie = (data: any, genreMap: Map<number, string>): Movie => {
   // const genres = data.genre_ids && data.genre_ids.map((id: number) => genreMap.get(id) || 'Unknown Genre');
   const genres = Array.isArray(data.genres)
     ? data.genres.map((genre: { id: number, name: string }) => genreMap.get(genre.id) || genre.name || 'Unknown Genre')
     :data.genre_ids ? data.genre_ids.map((id: number) => genreMap.get(id) || 'Unknown Genre') : [];
-console.log(genreMap)
-  return {
+ return {
     id: data.id,
-    poster: data.poster_path ? `https://image.tmdb.org/t/p/w500${data.poster_path}` : optionalPoster,
+    poster: data.poster_path ? `https://image.tmdb.org/t/p/w500${data.poster_path}` : '',
     title: data.title || 'Unknown Title',
     overview: data.overview,
     releaseYear: data.release_date ? new Date(data.release_date + "T00:00:00").getFullYear() : 0,
